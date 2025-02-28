00becc66070b981391f44eb948b656138e768f94
Nishant <sinha.nishant@outlook.com>
2025-03-01 00:22:56 +0530

    Chagne to One pass

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          One Pass Demo
         </a>
       </header>
     </div>
